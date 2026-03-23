cf4b24c181acbe09b4e68140d2125a3760235abf
Piyush <agarwalpiyush891@gmail.com>
2026-03-23 21:53:12 +0530

    ai-providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,12 @@ const Page = () => {
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
+  const testAI = useMutation(
+    trpc.testAI.mutationOptions({
+      onSuccess: () => toast.success("AI Job Queued"),
+    }),
+  );
+
   const create = useMutation(
     trpc.createWorkflow.mutationOptions({
       onSuccess: () => toast.success("Job Queued"),
@@ -21,6 +27,11 @@ const Page = () => {
     <div className="min-h-screen min-w-screen flex flex-col items-center gap-3 justify-center">
       <div>Protected Server Component</div>
       <div>{JSON.stringify(data)}</div>
+
+      <Button disabled={testAI.isPending} onClick={() => testAI.mutate()}>
+        Test AI
+      </Button>
+
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create Workflow
       </Button>
